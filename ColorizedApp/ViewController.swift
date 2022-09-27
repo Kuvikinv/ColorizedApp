f881340976176670461d240a9216d2ed6f3b848c
Kuvikinv <kuvikinvova@gmail.com>
2022-09-27 21:57:49 +0700

    Voprosi

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultColorView: UIView!
+    @IBOutlet var valueSliderRed: UILabel!
+    @IBOutlet var valueSliderGreen: UILabel!
+    @IBOutlet var valueSliderBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSliderRed()
+        setupSliderGreen()
+        setupSliderBlue()
+        valueSliders()
+        
+        resultColorView.layer.cornerRadius = 20
     }
+    @IBAction func sliderActionRed() {
+        var flexx = String(sliderRed.value)
+        flexx = String(format: "%.2f", flexx)
+        valueSliderRed.text = flexx
+        resultColorView.tintColor = sliderRed.minimumTrackTintColor?.withAlphaComponent(CGFloat(sliderRed.value))
+    }
+    @IBAction func sliderActionGreen() {
+        valueSliderGreen.text = String(sliderGreen.value)
+        resultColorView.tintColor = sliderGreen.minimumTrackTintColor?.withAlphaComponent(CGFloat(sliderGreen.value))
+    }
+    @IBAction func sliderActionBlue() {
+        valueSliderBlue.text = String(sliderBlue.value)
+        resultColorView.tintColor = sliderBlue.minimumTrackTintColor?.withAlphaComponent(CGFloat(sliderBlue.value))
+    }
+    
 
-
+    // MARK: - Private Methods
+    
+    private func valueSliders() {
+        valueSliderRed.text = String(sliderRed.value)
+        valueSliderGreen.text = String(sliderGreen.value)
+        valueSliderBlue.text = String(sliderBlue.value)
+    }
+    private func setupSliderRed() {
+        sliderRed.value = 0.1
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 1
+        sliderRed.minimumTrackTintColor = .red
+    }
+    private func setupSliderGreen() {
+        sliderGreen.value = 0.2
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 1
+        sliderGreen.minimumTrackTintColor = .green
+    }
+    private func setupSliderBlue() {
+        sliderBlue.value = 0.3
+        sliderBlue.minimumValue = 0
+        sliderBlue.maximumValue = 1
+        sliderBlue.minimumTrackTintColor = .blue
+    }
 }
 
